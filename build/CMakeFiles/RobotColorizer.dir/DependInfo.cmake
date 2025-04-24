ac6f4855e0197b4ca3b8c80109fbdaf506e7b6f5
ArtemOtr <nenado.t@yandex.ru>
2025-04-24 18:36:34 +0300

    ээээээ

---
+++ build/CMakeFiles/RobotColorizer.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/allabaichik/МАИ ДЗ/RobotColorizer/colorizer.cpp" "CMakeFiles/RobotColorizer.dir/colorizer.cpp.o" "gcc" "CMakeFiles/RobotColorizer.dir/colorizer.cpp.o.d"
+  "/home/allabaichik/МАИ ДЗ/RobotColorizer/main.cpp" "CMakeFiles/RobotColorizer.dir/main.cpp.o" "gcc" "CMakeFiles/RobotColorizer.dir/main.cpp.o.d"
+  "/home/allabaichik/МАИ ДЗ/RobotColorizer/mqtt_handler.cpp" "CMakeFiles/RobotColorizer.dir/mqtt_handler.cpp.o" "gcc" "CMakeFiles/RobotColorizer.dir/mqtt_handler.cpp.o.d"
+  "/home/allabaichik/МАИ ДЗ/RobotColorizer/robot_controller.cpp" "CMakeFiles/RobotColorizer.dir/robot_controller.cpp.o" "gcc" "CMakeFiles/RobotColorizer.dir/robot_controller.cpp.o.d"
+  "/home/allabaichik/МАИ ДЗ/RobotColorizer/server.cpp" "CMakeFiles/RobotColorizer.dir/server.cpp.o" "gcc" "CMakeFiles/RobotColorizer.dir/server.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
